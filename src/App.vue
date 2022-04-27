36a18cdd39818e56c51c0613d82233ea37bcc579
mingo <mingo233@qq.com>
2022-04-27 21:35:36 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+
+  <router-view />
+
+  <about msg="Hello Vue 3 + TypeScript + Vite" />
+</template>
+
+<script setup lang="ts">
+import { defineComponent } from "vue";
+import about from "@/components/ab.vue"; 
+
+ defineComponent({
+  name: "Home",
+  components: {
+    about,
+  },
+});
+</script>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/views/a.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="title">
+    hello 
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {
+    console.log(11);
+    
+  },
+})
+</script>
+
+<style lang="less" scoped>
+.title {
+  color: red;
+}
+</style>
