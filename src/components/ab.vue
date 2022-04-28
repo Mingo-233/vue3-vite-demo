a67f596d88250a5a3c41603fbc86cfeb50ade687
mingo <mingo233@qq.com>
2022-04-28 23:25:08 +0800

    feat: vue3

--- src/components/ab.vue
+++ src/components/ab.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button @click="emitHandle">emit</button>
   </div>
 </template>
 <script lang="ts">
@@ -15,8 +16,13 @@ export default defineComponent({
   setup(props, context) {
     console.log(11);
     console.log(props);
-
     console.log(context);
+    const emitHandle = () => {
+      context.emit("pick");
+    };
+    return {
+      emitHandle,
+    };
   },
 });
 </script>
--- src/views/index.vue
+++ src/views/index.vue
@@ -9,11 +9,28 @@
   </div>
   <div>
     组件
-    <Ab :visiable="true"></Ab>
+    <Ab :visiable="true" @pick="pickHandle" ref="ab1"></Ab>
+  </div>
+
+  <div>time - {{ time }}</div>
+
+  <div>statesAsRefs -- {{ foo }}</div>
+  {{ state }}
+
+  <div>
+    <input type="text" name="" id="ii" ref="vv1" />
   </div>
 </template>
 <script lang="ts">
-import { defineComponent, reactive, ref } from "vue";
+import {
+  computed,
+  defineComponent,
+  onMounted,
+  reactive,
+  ref,
+  toRefs,
+  watch,
+} from "vue";
 import Ab from "@/components/ab.vue";
 export default defineComponent({
   setup() {
@@ -28,12 +45,50 @@ export default defineComponent({
     const updatePersonInfo = () => {
       person.age = ~~(Math.random() * 10);
     };
+
+    const pickHandle = () => {
+      console.log("pick 触发了");
+    };
+
+    const time = computed(() => Date.now());
+
+    watch(
+      num,
+      (newValue, oldValue) => {
+        console.log(newValue, oldValue);
+      },
+      { immediate: true, deep: true }
+    );
+
+    const state = reactive({
+      foo: "a",
+      bar: "b",
+    });
+    const stateAsRefs = toRefs(state);
+    console.log(stateAsRefs);
+
+    const vv1 = ref<HTMLElement | null>(null);
+    const ab1 = ref<HTMLElement | null>(null);
+
+    onMounted(() => {
+      let ii = document.getElementById("ii") as HTMLInputElement;
+      console.log("mounted");
+      console.log(ii);
+
+      console.log(vv1.value);
+      console.log(ab1.value);
+    });
+
     return {
       msg: "hello world",
       num,
       addHandle,
       person,
       updatePersonInfo,
+      pickHandle,
+      time,
+      state,
+      ...stateAsRefs,
     };
   },
   components: {
